9b23aec25c15c5766038476c365f9a7179272535
Ayce45 <evan.j@hotmail.fr>
2021-03-26 16:26:12 +0100

    🐛 highlight ✨ modal in components

---
+++ components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    v-if="modalVisibility"
+    class="h-screen w-full fixed flex items-center justify-center bg-modal z-10"
+  >
+    <div
+      class="bg-white rounded shadow p-8 m-4 max-w-lg max-h-full text-center"
+    >
+      <div class="mb-4">
+        <h1
+          class="text-4xl sm:text-5xl md:text-7xl font-bold text-gray-600 mb-5"
+        >
+          Welcome!
+        </h1>
+      </div>
+      <div class="mb-8">
+        <ul class="text-gray-400">
+          <li>🔙 APOD for background</li>
+          <li>🧠 Search input</li>
+          <li>💖 Store for love</li>
+          <li>💄 Tailwind for css</li>
+          <li>🪐 Images size by planet size</li>
+          <li>🧪 Jest for test</li>
+        </ul>
+      </div>
+      <div class="flex justify-center">
+        <button
+          class="flex-no-shrink text-white py-2 px-4 rounded bg-green-400 hover:bg-green-600 transition-all"
+          @click="$store.commit('modal/hide')"
+        >
+          Let's Go
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  computed: {
+    modalVisibility() {
+      return this.$store.state.modal.visible
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,40 +5,7 @@
       background: 'url(' + apod.url + ') no-repeat center center fixed',
     }"
   >
-    <div
-      v-if="modalVisibility"
-      class="h-screen w-full fixed flex items-center justify-center bg-modal z-10"
-    >
-      <div
-        class="bg-white rounded shadow p-8 m-4 max-w-lg max-h-full text-center"
-      >
-        <div class="mb-4">
-          <h1
-            class="text-4xl sm:text-5xl md:text-7xl font-bold text-gray-600 mb-5"
-          >
-            Welcome!
-          </h1>
-        </div>
-        <div class="mb-8">
-          <ul class="text-gray-400">
-            <li>🔙 APOD for background</li>
-            <li>🧠 Search input</li>
-            <li>💖 Store for love</li>
-            <li>💄 Tailwind for css</li>
-            <li>🪐 Images size by planet size</li>
-            <li>🧪 Jest for test</li>
-          </ul>
-        </div>
-        <div class="flex justify-center">
-          <button
-            class="flex-no-shrink text-white py-2 px-4 rounded bg-green-400 hover:bg-green-600 transition-all"
-            @click="$store.commit('modal/hide')"
-          >
-            Let's Go
-          </button>
-        </div>
-      </div>
-    </div>
+    <Modal />
     <div
       :class="{
         'opacity-0': modalVisibility,
@@ -183,7 +150,7 @@
               </div>
             </div>
             <div class="p-5">
-              <h3 v-highlight="search" class="text-white text-lg">
+              <h3 class="text-white text-lg">
                 {{ planet.title }}
               </h3>
               <p class="text-gray-400 description">
